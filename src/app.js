7be25c19b03855a51f452d9d3e0cc345e789f9eb
Felipe Melo <narulee@hotmail.com>
2020-07-25 13:31:04 -0300

    edit about page

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
+        title: 'About',
         developer: 'Felipe Melo',
         img: 'img/dev.png'
     })
